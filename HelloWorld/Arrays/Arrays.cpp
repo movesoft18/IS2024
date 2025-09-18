b2f5961b2d8f890a68419f19764f6e307e476f0d
movesoft18 <movesoft@yandex.ru>
2025-09-18 13:38:36 +0400

    arrays

---
+++ HelloWorld/Arrays/Arrays.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    int numbers[10] = {-1,2,30,4,5,66,7,8,9,0};
+    int numbers1[]{9,8,7,6,5,4,-3};
+    int a = numbers[2];
+    numbers[9] = 100;
+    char s1[] = {'h','e','l','l','o','!'};
+    char s2[] = "hello!";
+    int sizeNumbers = sizeof(numbers1) / sizeof(int);
+
+    for (int i = 0; i < sizeNumbers; i+=2)
+        cout << numbers1[i] << " ";
+    cout << endl;
+    int sum = 0;
+    for (int i = 0; i < sizeNumbers; i++)
+    {
+        sum += numbers[i];
+    }    
+    cout << "Сумма элементов массива numbers = " << sum << endl;
+
+    for (auto element : numbers)
+        cout << element << " ";
+    cout << endl;
+
+
+
+    cout << endl;
+
+    
+}
+
---
+++ HelloWorld/Arrays2/Arrays2.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    int matrix[3][2]{
+        {1,4},
+        {6,3},
+        {-5, 12}
+    };
+
+    cout << matrix[2][1] << endl;
+
+    double m[3][3]{
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+
+    // i - номер строки
+    for (int i = 0; i < 3; i++)
+    {
+        // j - столбец
+        for (int j = 0; j < 3; j++)
+        {
+            cin >> m[i][j];
+        }
+    }
+
+
+    // i - номер строки
+    for (int i = 0; i < 3; i++)
+    {
+        // j - столбец
+        for (int j = 0; j < 3; j++)
+        {
+            cout << m[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    double determinant = m[0][0] * m[1][1] * m[2][2] +
+        m[0][1] * m[1][2] * m[2][0] +
+        m[1][0] * m[2][1] * m[0][2] -
+        m[0][2] * m[1][1] * m[2][0] -
+        m[0][1] * m[1][0] * m[2][2] -
+        m[2][1] * m[1][2] * m[0][0];
+    cout << "Определитель = " << determinant << endl;
+
+}
+
