7c1d10c5b570aa54871fa6bdf8b27c23755468b3
movesoft18 <movesoft@yandex.ru>
2025-09-25 13:38:44 +0400

    Функции

---
+++ HelloWorld/Factorial/Factorial.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+long long factorial(int n)
+{
+	unsigned long long f = 1;
+	for (int i = 2; i <= n; i++)
+	{
+		f *= i;
+	}
+	return f;
+}
+
+long long sum(int n)
+{
+	unsigned long long f = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		f += i;
+	}
+	return f;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	//cout << "Программа вычисления факториала n!\n";
+	//cout << "Введите n: ";
+	//int n;
+	//cin >> n;
+	//cout << "Факториал " << n << 
+	//	"! = " << factorial(n) << endl;
+
+	for (int i = 0; i <= 15; i++)
+		cout << "Факториал " << i <<
+			"! = " << factorial(i) << endl;
+	for (int i = 1; i <= 15; i++)
+		cout << "Сумма чисел от 1 до " << i <<
+		" = " << sum(i) << endl;
+}
+
---
+++ HelloWorld/Functions/Functions.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+
+int x, int y;
+/*
+void sum()
+{
+    double a, b, c;
+    std::cout << "Программа сложения двух чисел.\n";
+    std::cout << "Введите 1-е число: ";
+    std::cin >> a;
+    std::cout << "Введите 2-е число: ";
+    std::cin >> b;
+    if (a < 0 || b < 0)
+        return;
+    c = a + b;
+    std::cout << a << " + " << b << " = " << c << "\n";
+    return;
+}
+*/
+
+int sum(int a, int b)
+{
+    int c = a + b;
+    return c;
+}
+
+
+void printResult(int a, int b, int result)
+{
+    std::cout << a << " + " << b << " = "
+        << result << "\n";
+}
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    double a, b;
+    std::cout << "Программа сложения двух чисел.\n";
+    std::cout << "Введите 1-е число: ";
+    std::cin >> a;
+    std::cout << "Введите 2-е число: ";
+    std::cin >> b;
+    sum(9, 4);
+    int result = sum(5, 7);
+    printResult(a, b, result);
+
+    //int result1 = sum(result, 70);
+
+}
