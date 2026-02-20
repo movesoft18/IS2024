49dc4bc64e9f3b2a55e66d91e8566a6d8e3376b9
movesoft18 <movesoft@yandex.ru>
2026-02-20 10:43:53 +0400

    list

---
+++ List/ListDemo/ListDemo.cpp
@@ -0,0 +1,41 @@
+﻿#include "ListLib.h"
+#include <iostream>
+
+ListItem* first = nullptr;
+int main()
+{
+    setlocale(LC_ALL, "");
+    AddFirst(first, 1, "Яблоки", 93.99, 100); // добавляем в начало первый элемент Яблоки
+    AddFirst(first, 3, "Бананы", 72, 90);   // добавляем в начало второй элемент Бананы, бананы стают первыми, Яблоки вторыми
+    auto oranges = AddLast(first, 4, "Апельсины", 89.99, 50.7); // добавляем в конец Апельсины, oranges - указатель на апельсины
+    auto mandarine = AddLast(first, 5, "Мандарины", 189.99, 147);// добавляем в конец Мандарины
+    PrintList(first);
+    auto kiwi = AddAfter(oranges, 2, "Киви", 129.99, 17); // вставить после апельсинов
+    auto persimmon = AddAfter(kiwi, 6, "Хурма", 159.99, 27); // вставить после киви
+    PrintList(first);
+    auto i2 = Delete(first, kiwi); // удаляем киви
+    PrintList(first);
+    auto bananas2 = AddLast(first, 9, "Бананы", 160, 43);
+    PrintList(first);
+    std::cout << "---------------\n";
+    auto result = Find(first, "Бананы");
+    if (result == nullptr)
+        std::cout << "Товар не найден!\n";
+    while (result != nullptr)
+    {
+        PrintItem(result);
+        result = Find(result->next, "Бананы");
+    }
+    std::cout << "---------------\n";
+    auto bananasList = FindAll(first, "Бананы");
+    PrintList(bananasList);
+    std::cout << "---------------\n";
+    SaveListToTextFile(first, "list.txt");
+    SaveListToTextFile(bananasList, "bananas.txt");
+    DeleteList(bananasList);
+    DeleteList(first);
+    first = LoadListFromTextFile("list.txt");
+    PrintList(first);
+    DeleteList(first);
+}
+
---
+++ List/ListExample/ListExample.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+using namespace std;
+
+struct ListItem
+{
+    int id; // идентификатор товара
+    char name[30];  // название
+    float price; // цена за единицу
+    float count; // количество
+    ListItem* next; // указатель на следующий элемент
+};
+
+ListItem* first = nullptr;
+
+// обновить значение этой глобальной переменной first
+void AddFirst(ListItem*& firstItem,
+    int productId, 
+    const char* productName, 
+    float productPrice, 
+    float productCount)
+{
+    ListItem* temp = new ListItem; // создаем в памяти новый элемент списка
+    temp->id = productId;   // присваиваем полям элемента нужные значения
+    strcpy_s(temp->name, productName); // имя продукта - это стока C-Style, поэтому копируем ее с помощью специальной функции копирования строк
+    temp->price = productPrice; // присваиваем полям элемента нужные значения
+    temp->count = productCount; // присваиваем полям элемента нужные значения
+    temp->next = firstItem; // нам нужно встроить элемент в начало списка, поэтому
+    // указатель next настраиваем таким образом, чтобы он указывал на элемент
+    // который ранее был первым (на него указывает first)
+    firstItem = temp;       // а теперь делаем, чтобы first указывал на наш созданный элемент
+}
+
+void PrintItem(ListItem* item)
+{
+    if (item == nullptr) return; // проверяем коректность указателя
+    cout << "Ид товара: " << item->id << endl; // далее выводим данные в консоль
+    cout << "Название: " << item->name << endl;
+    cout << "Цена: " << item->price << " руб." << endl;
+    cout << "Количество: " << item->count << " кг." << endl;
+    cout << endl;
+}
+
+void PrintList(ListItem* firstItem)
+{
+    auto temp = firstItem; // начинаем с первого элемента
+    while (temp != nullptr) // перебираем весь список
+    {
+        PrintItem(temp); // печатаем очередной элемент ранее созданной функцией
+        temp = temp->next; // перемещаемся к следующему элементу
+    }
+}
+
+int main()
+{
+    setlocale(LC_CTYPE, "");
+    AddFirst(first, 1, "Яблоки", 150, 30.5);
+    AddFirst(first, 2, "Груши", 250, 180);
+    AddFirst(first, 3, "Бананы", 170, 3);
+    PrintList(first);
+}
