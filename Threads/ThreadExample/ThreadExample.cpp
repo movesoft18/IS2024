31d552e4ce76578df645555dd08a6ef0e5fa7b8f
movesoft18 <movesoft@yandex.ru>
2026-04-13 09:49:34 +0400

    threadbase

---
+++ Threads/ThreadExample/ThreadExample.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <thread>
+
+// Функция, которая будет выполняться в отдельном потоке
+void threadFunction() {
+    std::cout << "Привет из потока с ID: " << std::this_thread::get_id() << std::endl;
+}
+
+int main() {
+    setlocale(LC_ALL, "");
+    // Создаём поток и передаём ему функцию для выполнения
+    std::thread t(threadFunction);
+    std::thread t2(threadFunction);
+    std::thread t3(threadFunction);
+    std::thread t4(threadFunction);
+
+    // Выводим ID основного потока
+    std::cout << "Привет из основного потока с ID: " << std::this_thread::get_id() << std::endl;
+
+    // Ждём завершения потока (если не вызвать, программа может завершиться до завершения потока)
+    t.join();
+    t2.join();
+    t3.join();
+    t4.join();
+
+    return 0;
+}
